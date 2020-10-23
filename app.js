2e5d7783a30948315efd8a48671822f1069284df
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-10-23 16:01:00 +0200

    fixed line breaks in the console

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ for (let i = 0; i < friends.length; i++) {
     } else {
       chorus += `${j} lines of code in the file, ${j} lines of code; ${
         friends[i]
-      } strikes one out, clears it all out, ${j - 1} lines of code in the file
-      `;
+      } strikes one out, clears it all out, ${
+        j - 1
+      } lines of code in the file\n`;
     }
   }
 
-  console.log(friends[i] + ':\n', chorus);
-  //   console.log(`${friends[i]}:
-  //       ${chorus}`);
+  // console.log(friends[i] + ':\n', chorus);
+  console.log(`${friends[i]}:\n${chorus}`);
 }
